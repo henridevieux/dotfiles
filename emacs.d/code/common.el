9aca0efef48cb22684d6c466710e4f7c95e3f6fd
Henri Devieux <henridevieux@users.noreply.github.com>
2021-10-11 23:41:16 -0400

    Add gopls integration, simplify go config.

--- emacs.d/code/common.el
+++ emacs.d/code/common.el
@@ -1,18 +1,19 @@
-;; (use-package auto-complete
-;;   :ensure t
-;;   :init
-;;   (progn
-;;     (ac-config-default)
-;;     (auto-complete-mode t))
-;;   :config
-;;   (progn
-;;     (ac-set-trigger-key "TAB")
-;;     (setq ac-modes '(c-mode c++-mode python-mode rust-mode))))
-(use-package auto-complete
+(use-package company
   :ensure t
-  :diminish auto-complete-mode
-  :init
-    (ac-config-default))
+  :defer t
+  :init (global-company-mode)
+  :config
+  (progn
+    (setq company-tooltip-align-annotations t
+          company-show-numbers t)
+    (setq company-dabbrev-downcase nil))
+  :diminish company-mode)
+
+(use-package yasnippet
+  :ensure t
+  :config
+  (add-to-list 'yas-snippet-dirs "~/.emacs.d/snippets")
+  (yas-global-mode 1))
 
 (use-package whitespace
   :ensure t
@@ -25,16 +26,16 @@
     (setq whitespace-line-column 100)
   (setq whitespace-style '(face tabs trailing lines-tail)))
 
-(use-package flycheck
-  :ensure t
-  :diminish flycheck-mode
-  :init
-  (global-flycheck-mode)
-  :config
-  (progn
-    (setq-local flycheck-highlighting-mode nil)
-    (setq-default flycheck-flake8-maximum-line-length 120)
-    (setq-local flycheck-check-syntax-automatically nil)))
+;; (use-package flycheck
+  ;; :ensure t
+  ;; :diminish flycheck-mode
+  ;; :init
+  ;; (global-flycheck-mode)
+  ;; :config
+  ;; (progn
+    ;; (setq-local flycheck-highlighting-mode nil)
+    ;; (setq-default flycheck-flake8-maximum-line-length 120)
+    ;; (setq-local flycheck-check-syntax-automatically nil)))
 
 (use-package smartparens
   :ensure t
@@ -43,3 +44,10 @@
   (progn
     (use-package smartparens-config)
     (smartparens-global-mode 1)))
+
+(use-package lsp-mode
+  :ensure t
+  :init
+  ;; (setq lsp-keymap-prefix "C-c l")
+  :hook (go-mode . lsp-deferred)
+  :commands (lsp lsp-deferred))
--- emacs.d/code/go.el
+++ emacs.d/code/go.el
@@ -1,23 +1,13 @@
+(setenv "GOPATH" "/Users/hdevieux/gocode")
+(setenv "GOROOT" "/usr/local/go")
+(setq exec-path (append exec-path '("/Users/hdevieux/gocode/bin" "/user/local/go/bin")))
+(setenv "PATH" (concat (getenv "PATH") ":/Users/hdevieux/gocode/bin:/usr/local/go/bin"))
+
 (use-package go-mode
   :ensure t
   :mode ("\\.go$" . go-mode)
-  :config
-  (add-hook 'before-save-hook #'gofmt-before-save)
-  (setenv "GOPATH" "/Users/hdevieux/gocode")
-  (auto-complete-mode t)
-  (setq exec-path (append exec-path '("/Users/hdevieux/gocode/bin")))
-  (setq exec-path (append exec-path '("/usr/local/go/bin")))
-  (setq gofmt-command "goimports")
-  (setq whitespace-line-column 120)
-  (use-package go-eldoc
-    :ensure t
-    :init
-    (add-hook 'go-mode-hook 'go-eldoc-setup)
-    :config
-    (setq go-eldoc-gocode "/usr/local/go/bin/go"))
-  ;; Delete this line and uncomment below
-  )
-  ;; (use-package go-autocomplete
-  ;;   :ensure t
-  ;;   :config
-  ;;   (setq ac-go-gocode-bin "/usr/local/go/bin/go")))
+  :hook ((go-mode . lsp-deferred)
+	 (before-save . lsp-format-buffer)
+	 (before-save . lsp-organize-imports)))
+
+(provide 'gopls-config)
